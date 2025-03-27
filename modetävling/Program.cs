ffb0ee745902f059b252ec4fa997c558214a4a4e
jasmine lindberg <Jasmine.lindberg@elev.ga.ntig.se>
2025-03-27 12:55:53 +0100

    oiodjoj

--- modetävling/Program.cs
+++ modetävling/Program.cs
@@ -17,21 +17,61 @@ Console.WriteLine("De första man bör göra är att välja en topp");
 
 
 Console.WriteLine("Du kan välja mellan:");
-String [] toppar = [ "gucci tröja", "Glittrig balenciaga topp", "svättig tröja" , "köttklänning"];
+// String [] toppar = [ "gucci tröja", "Glittrig balenciaga topp", "svättig tröja" , "köttklänning"];
 
-for (int i = 0; i < toppar.Length; i++)
+for (int i = 0; i < 1; i++)
 {
-    Console.WriteLine(toppar[0]);
-    Console.WriteLine(toppar[1]);
-    Console.WriteLine(toppar[2]);
-    Console.WriteLine(toppar[3]);
+  Console.WriteLine("1) gucci topp");
+  Console.WriteLine("2) balenciaga skjorta");
+  Console.WriteLine("3) H&M t-shirt");
+  Console.WriteLine("4) svettig tröja");
+  Console.WriteLine("5) inget(; ");
+
+
+}
+
+Console.WriteLine("vilket plagg väljer du");
+string whatToChoose = Console.ReadLine();
+while (whatToChoose != "1" && whatToChoose != "2" && whatToChoose != "3" && whatToChoose != "4" && whatToChoose != "5") ;
+{
+  whatToChoose = Console.ReadLine();
 }
 
 
+Random generator = new Random();
+
+int poäng = 0;
+
+if (whatToChoose == "1")
+{
+
+  poäng = Random.Shared.Next(10);
+}
+else if (whatToChoose == "2")
+{
+  poäng = Random.Shared.Next(10);
+}
+else if (whatToChoose == "3")
+{
+  poäng =Random.Shared.Next(10);
+}
+else if (whatToChoose == "4")
+{
+  poäng = Random.Shared.Next(10);
+}
+else if (whatToChoose == "5")
+{
+  poäng = Random.Shared.Next(10);
+}
 
 
+Console.Clear();
 
 
+Console.WriteLine("Nästa steg! Nu är de dags att välja underdel");
+Console.WriteLine("Du har dessa att välja på:");
+
+strek(); 
 
 
 
